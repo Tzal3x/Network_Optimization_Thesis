e349e39bbde0ce3a7146da098fe0f3a4e9afd9f7
Alexandros Tzimas <tzalex@outlook.com.gr>
2019-03-12 01:54:51 +0200

    Added more satelites (and stations)
    Still remains  a bug of overlapping station "orbits". The stations despite being initialized at different positions, in the for loop they apear at the same coordinates again and as a result only one of them is displayed (or more precisely, both are displayed while overlapping each other)

--- part1-satelites-stations/satelite.m
+++ part1-satelites-stations/satelite.m
@@ -1,26 +1,22 @@
 classdef satelite < handle %why do i have to use as a base class the handle one???
-    %{
-    Contains all the information about a satelite-node.
-    Each satelite has a constant communication range, therefore the satelite can be
-    expressed as the center of a sphere and the communication as the radius of the sphere.
-    %}
-    %% PROPERTIES
+    %initialize as: satelite(arg_vel,arg_alt,arg_steps)
+    %Contains all the information about a satelite-node.
+    %Class describing a satelite object. Every satelite is also a source in graph
+    %terms.
+    
+    % PROPERTIES
     properties(SetAccess = private) %Only satelite's methods must be able to access these
-       init_long = 0 %(degrees), initial longitude, using this in order to have a point of reference
-       init_lat = 0 %(degrees), initial latitude, using this in order to have a point of reference
-       longitude = 0 % current longitude of satelite
-       latitude = 0 % current latitude of satelite
-       satelite_id = 0  % change this. It must be an increasing number from 1 to N
+       longitude = 0 % current longitude of satelite (initialised as cartesian, after first call of net_pos it is transdormed to polar)
+       latitude = 0 % current latitude of satelite (initialiased as cartesian, after first call of net_pos it is transdormed to polar)
        angular_velocity = 0 % measured in degrees/sec. Float number, it can take both positive and negative values. https://en.wikipedia.org/wiki/Orbital_speed
+       satelite_id = 0  % CHANGE THIS. It must be an increasing number from 1 to N
+       altitude = 0; % 408.000 meters (like the ISS)
     end
-   properties(Constant)
-       altitude = 408; % 408.000 meters (like the ISS)
-   end
    
    properties %public properties
        neighbours = [] %binary list (of size N+M) containing the other sources (or sinks) that have a connection with the satelite.
    end
-   %% METHODS
+   % METHODS
    methods
        % --- Getters
        function out = get_lat(obj)%returns current latitude
@@ -31,19 +27,17 @@ classdef satelite < handle %why do i have to use as a base class the handle one?
            out = obj.longitude;
        end
        
-       function sat_obj = satelite(arg_long,arg_lat,arg_vel) %constructor, input arguments: longitude, latitude, angular velocity
-          if nargin == 3 %if number of variables (...). If this statement is skipped, initialising the constructor with less than 3 errors will cause an error
-            sat_obj.init_long = arg_long;
-            sat_obj.init_lat = arg_lat;
+       function sat_obj = satelite(arg_vel,arg_alt,arg_steps) %constructor, input arguments: longitude, latitude, angular velocity
             sat_obj.angular_velocity = arg_vel;
-          end
+            sat_obj.altitude = arg_alt;
+            sat_obj.longitude = arg_alt*cosd(arg_vel*arg_steps);
+            sat_obj.latitude = arg_alt*sind(arg_vel*arg_steps);
        end
        
-       function next_pos(obj, step) %calculate and update the satelite's polar coordinates given it's angular velocity
-           %step: next step (in degrees) at time t+1
+       function next_pos(obj, steps) %calculate and update the satelite's polar coordinates given it's angular velocity
            radius = obj.altitude;
-           obj.longitude = radius*cosd(obj.init_long + step*obj.angular_velocity);%Long = X
-           obj.latitude = radius*sind(obj.init_lat + step*obj.angular_velocity); %Lat = Y 
+           obj.longitude = radius*cosd(steps*obj.angular_velocity);%Long = X (polar)
+           obj.latitude = radius*sind(steps*obj.angular_velocity); %Lat = Y (polar)
        end
    end
 end
